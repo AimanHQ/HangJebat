32cece39d57d9fda9b0c33326810d2388734226f
AimanHQ <haqimboy94@gmail.com>
2024-10-26 14:14:05 +0800

    Update
    -Placing some NPC to interact

--- Assets/Code/spells/Items/Weapon FX.cs
+++ Assets/Code/spells/Items/Weapon FX.cs
@@ -6,36 +6,48 @@ namespace HQ
 {
     public class WeaponFX : MonoBehaviour
     {
-        [Header("Weaapon FX")]
+        [Header("Weapon FX")]
         public ParticleSystem normalWeaponTrail;
+        public Transform playerTransform;  // Reference to the player's transform
+
         private Quaternion initialRotation;
-
-
-        //dark weapon trail
-        //magic weapon trail
+        private Vector3 initialOffset;
 
         void Start()
         {
-            // Store the initial rotation of the particle system
-            initialRotation = normalWeaponTrail.transform.rotation;
+            if (normalWeaponTrail != null && playerTransform != null)
+            {
+                // Store the initial rotation of the particle system
+                initialRotation = normalWeaponTrail.transform.rotation;
+
+                // Calculate the initial offset of the particle system relative to the player
+                initialOffset = normalWeaponTrail.transform.position - playerTransform.position;
+            }
         }
 
         public void PlayWeaponFx()
         {
-            normalWeaponTrail.Stop();
-
-            if (normalWeaponTrail.isStopped)
+            if (normalWeaponTrail != null)
             {
-                normalWeaponTrail.Play();
+                normalWeaponTrail.Stop();
+
+                if (normalWeaponTrail.isStopped)
+                {
+                    // Play the particle system
+                    normalWeaponTrail.Play();
+                }
             }
         }
 
         void LateUpdate()
         {
-            // Lock the particle system rotation to its initial rotation
-            if (normalWeaponTrail != null)
+            if (normalWeaponTrail != null && playerTransform != null)
             {
+                // Lock the particle system rotation to its initial rotation
                 normalWeaponTrail.transform.rotation = initialRotation;
+
+                // Update the particle system position to follow the player's position with an offset
+                normalWeaponTrail.transform.position = playerTransform.position + initialOffset;
             }
         }
     }
